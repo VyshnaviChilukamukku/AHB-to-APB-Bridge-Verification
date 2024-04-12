c13579b120350c3a8c75ebc2c838622fc6ac30cd
Chilukamukku Vyshnavi <96568668+VyshnaviChilukamukku@users.noreply.github.com>
2024-04-12 20:45:18 +0530

    Create ahb_sequencer.sv

---
+++ AHB_agent/ahb_sequencer.sv
@@ -0,0 +1,12 @@
+class ahb_sequencer extends uvm_sequencer #(ahb_xtn);
+
+	`uvm_component_utils(ahb_sequencer)
+
+	extern function new(string name = "ahb_sequencer", uvm_component parent);
+
+endclass
+
+//constructor
+function ahb_sequencer::new(string name = "ahb_sequencer", uvm_component parent);
+	super.new(name, parent);
+endfunction
