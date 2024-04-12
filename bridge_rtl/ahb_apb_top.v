6b9fb9592e22a09a926ff359e785b5334a5cab27
Chilukamukku Vyshnavi <96568668+VyshnaviChilukamukku@users.noreply.github.com>
2024-04-12 21:31:09 +0530

    Update ahb_apb_top.v

--- bridge_rtl/ahb_apb_top.v
+++ bridge_rtl/ahb_apb_top.v
@@ -1,31 +1,4 @@
-/********************************************************************************************
 
-Copyright 2011-2012 - Maven Silicon Softech Pvt Ltd. All Rights Reserved.
-
-This source code is an unpublished work belongs to Maven Silicon Softech Pvt Ltd.
-It is considered a trade secret and is not to be divulged or used by parties who 
-have not received written authorization from Maven Silicon Softech Pvt Ltd.
-
-Maven Silicon Softech Pvt Ltd
-Bangalore - 560076
-
-Webpage: www.maven-silicon.com
-
-Filename:	ahb_apb_top.v   
-
-Description:    AHB-APB bridge top integrates AHB Slave & APB Controller
-
-Date:		27/03/2013
-
-Author:		Susmita Nayak
-
-Email:		siva@maven-silicon.com
-		siva@aceic.com
-
-Version:	2.0
-
-*********************************************************************************************/
-// Include definitions
 `include "definitions.v"
    module rtl_top (input  Hclk,
                    input  Hresetn,
